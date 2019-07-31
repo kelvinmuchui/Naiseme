e173436f3aa44c06163d059f373e124d3f657ea1
kelvinmuchui <muchuikelvin423@gmail.com>
2019-07-31 19:00:10 +0300

    Police report

---
+++ EventReporter/app/src/main/java/com/example/hamburger_w/eventreporter/MainActivity.java
@@ -0,0 +1,153 @@
+/**
+ * Login and register
+ */
+
+package com.example.hamburger_w.eventreporter;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView logo, ivSignIn, btnTwitter;
+    private AutoCompleteTextView email, password;
+    private TextView forgotPass, signUp;
+    private Button btnSignIn;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeGUI();
+
+        user = firebaseAuth.getCurrentUser();
+
+        if(user != null) {
+            finish();
+            startActivity(new Intent(MainActivity.this,EventActivity.class));
+        }
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String inEmail = email.getText().toString();
+                String inPassword = password.getText().toString();
+
+                if(validateInput(inEmail, inPassword)){
+                    signUser(inEmail, inPassword);
+                }
+
+            }
+        });
+
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Register.class));
+            }
+        });
+
+//        forgotPass.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(LoginActivity.this,PWresetActivity.class));
+//            }
+//        });
+
+
+
+    }
+
+
+
+    public void signUser(String email, String password){
+
+        progressDialog.setMessage("Verificating...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,EventActivity.class));
+                }
+                else{
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"Invalid email or password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    private void initializeGUI(){
+
+        logo = findViewById(R.id.ivLogLogo);
+        ivSignIn = findViewById(R.id.ivSignIn);
+        btnTwitter = findViewById(R.id.ivFacebook);
+        email = findViewById(R.id.atvEmailLog);
+        password = findViewById(R.id.atvPasswordLog);
+        forgotPass = findViewById(R.id.tvForgotPass);
+        signUp = findViewById(R.id.tvSignIn);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+
+    public boolean validateInput(String inemail, String inpassword){
+
+        if(inemail.isEmpty()){
+            email.setError("Email field is empty.");
+            return false;
+        }
+        if(inpassword.isEmpty()){
+            password.setError("Password is empty.");
+            return false;
+        }
+
+        return true;
+    }
+
+}
